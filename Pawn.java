93b5163e2487c56a9558f501f43cd90b05c72552
Richard Wu <wu.richard.98@gmail.com>
2019-02-15 23:20:19 -0500

    Created empty functions for all different pawn moves

--- Pawn.java
+++ Pawn.java
@@ -1,12 +1,25 @@
 public class Pawn extends Piece{
 
+  private Boolean doubleMoved = False;
+
   public Pawn(int x, int y) {
     super(x,y);
   }
 
+  public void capture(int x, int y) {
+    ;
+  }
+
+  public void en_passant(int x, int y) {
+    ;
+  }
+
   @Override
-  void move(int x, int y) {
-    
+  public void move(int x, int y) {
+    if (x == this.x && y = this.y + 1) {
+      this.y = y;
+    }
+    return;
   }
 
 
