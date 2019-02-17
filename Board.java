81b33cea0364c2c23ec7782e98698248cb49849f
Richard Wu <wu.richard.98@gmail.com>
2019-02-17 00:25:59 -0500

    Initial commit

---
+++ Board.java
@@ -0,0 +1,27 @@
+public class Board {
+
+  public static final int BOARD_WIDTH = 8;
+  public static final int BOARD_HEIGHT = 8;
+
+  private Square[][] board;
+
+  public Board() {
+    //create and populate 2D array of Squares
+    board = new Square[BOARD_HEIGHT][BOARD_HEIGHT];
+    for (int i = 0; i < BOARD_HEIGHT; i++) {
+      for (int j = 0; j < BOARD_WIDTH; j++) {
+        board[i][j] = new Square(j, BOARD_HEIGHT-1-i);
+      }
+    }
+  }
+
+  public void print_board() {
+    for (int i = 0; i < BOARD_HEIGHT; i++) {
+      for (int j = 0; j < BOARD_WIDTH; j++) {
+        System.out.print("[" + board[i][j].getX() + "," + board[i][j].getY() +"]   ");
+      }
+      System.out.println();
+    }
+  }
+
+}
