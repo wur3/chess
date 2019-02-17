b292bb5908a9f9a6a01a824608a78e5fc6abf80a
Richard Wu <wu.richard.98@gmail.com>
2019-02-16 23:43:54 -0500

    Initial commit

---
+++ Square.java
@@ -0,0 +1,12 @@
+public class Square {
+
+  private boolean occupied = false;
+  private Piece piece = null;
+
+  private int x, y;
+
+  public Square(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
