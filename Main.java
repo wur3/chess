7a07cbf6d4186e84387c84afa3d2e8d614c06979
Richard Wu <wu.richard.98@gmail.com>
2019-02-17 00:25:47 -0500

    Testing board class

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
-  public static void main(String[] args) {
-    Pawn p1 = new Pawn(0,1);
-    Square[] board = new Square[];
 
-    System.out.println(p1.getX());
+  public static void main(String[] args) {
+    Board board = new Board();
+    board.print_board();
+
+
+
+
   }
 }
