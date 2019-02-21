b2c800eb2c89365bb6b2995c9f95edd1f177cd4a
Richard Wu <wu.richard.98@gmail.com>
2019-02-21 00:03:59 -0500

    testing atomic_move

--- Main.java
+++ Main.java
@@ -4,10 +4,13 @@ public class Main {
     Board board = new Board();
     board.print_xy();
     Pawn p1 = new Pawn(board,1,2,"black");
-    //System.out.println(board.piece_at(1,2));
-    Pawn p2 = new Pawn(board, 1,3,"black");
+    System.out.println(board.piece_at(1,2));
+    System.out.println(board.piece_at(1,3));
+    //Pawn p2 = new Pawn(board, 1,3,"black");
     p1.move(1,3);
-
+    System.out.println(board.piece_at(1,2));
+    System.out.println(board.piece_at(1,3));
+    System.out.println(board.piece_at(1,4));
 
 
 
