2772f1d2b8a0e40a398f2797d14569e9b5db0610
wur3 <wu.richard.98@gmail.com>
2019-02-22 13:44:55 -0500

    2 rows of pawns

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ public class Main {
 
   public static void main(String[] args) {
     Board board = new Board();
-    board.print_xy();
+    //board.print_xy();
+
+    /**
     Pawn p1 = new Pawn(board,1,2,"black");
     Pawn p2 = new Pawn(board, 2,3,"white");
     System.out.println(board.piece_at(1,2));
@@ -11,7 +13,18 @@ public class Main {
     p1.capture(2,3);
     System.out.println(board.piece_at(1,2));
     System.out.println(board.piece_at(2,3));
+    **/
+    Piece white_pieces[] = new Piece[16];
+    for (int i = 0; i < Board.BOARD_WIDTH; i++) {
+      white_pieces[i] = new Pawn(board, i, 1, "white");
+      System.out.println(white_pieces[i].toString());
+    }
 
+    Piece black_pieces[] = new Piece[16];
+    for (int i = 0; i < Board.BOARD_WIDTH; i++) {
+      black_pieces[i] = new Pawn(board, i, 6, "black");
+      System.out.println(black_pieces[i].toString());
+    }
 
 
   }
