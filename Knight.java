665c90f274f7b1cd67e72d2aa98e3b7f012e0498
wur3 <wu.richard.98@gmail.com>
2019-02-23 17:36:43 -0500

    Knight movement

--- Knight.java
+++ Knight.java
@@ -1,11 +1,41 @@
-public class Knight extends Piece{
+import java.util.Arrays;
 
+public class Knight extends Piece{
+  private int[] diff = {2, 1, -1, -2};
   public Knight(Board b, int x, int y, String color) {
     super(b, x, y, color);
   }
 
   @Override
   public void move(int x, int y) {
+    boolean x_in_range = false;
+    boolean y_in_range = false;
+
+    //check that both the x and y differences are 2,1,-1,or -2 but x and y also cannot be the same value
+    for (int i : diff) {
+      if (i == this.x-x) {
+        x_in_range = true;
+      }
+      else if (i == this.y-y) {
+        y_in_range = true;
+      }
+    }
+    if (!(x_in_range && y_in_range)) {
+      System.out.println("Knights cannot move like that.");
+      System.out.println(String.format("(%d,%d) to (%d,%d)",this.x,this.y,x,y));
+    }
+    //valid destination
+    else {
+      //empty space
+      if (!b.occupied_at(x,y)) {
+        atomic_move(x, y);
+      }
+      //enemy space
+      else if (b.piece_at(x,y).getColor() != color) {
+        b.clear(x, y);
+        atomic_move(x, y);
+      }
+    }
     return;
   }
 
--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
   public static void main (String[] args) {
     Board board = new Board();
-    /**]
+    /**
     //board.print_xy();
     Pawn p1 = new Pawn(board,1,2,"black");
     Pawn p2 = new Pawn(board, 2,3,"white");
@@ -12,14 +12,30 @@ public class Test {
     System.out.println(board.piece_at(1,2));
     System.out.println(board.piece_at(2,3));
 
-    **/
-    Rook p1 = new Rook(board,1,2,"black");
-    Rook p2 = new Rook(board, 5,2,"white");
+
+    Rook r1 = new Rook(board,1,2,"black");
+    Rook r2 = new Rook(board, 5,2,"white");
     System.out.println(board.piece_at(1,2));
     System.out.println(board.piece_at(5,2));
 
-    p1.move(5,2);
+    r1.move(5,2);
     System.out.println(board.piece_at(1,2));
     System.out.println(board.piece_at(5,2));
+
+
+    Bishop b1 = new Bishop(board,1,1,"white");
+    Bishop b2 = new Bishop(board,2,2,"black");
+    b1.move(2,2);
+    System.out.println(board.piece_at(1,1));
+    System.out.println(board.piece_at(2,2));
+    **/
+
+    Knight k1 = new Knight(board,1,1,"white");
+    Knight k2 = new Knight(board,2,3,"black");
+    System.out.println(board.piece_at(1,1));
+    System.out.println(board.piece_at(2,3));
+    k1.move(2,3);
+    System.out.println(board.piece_at(1,1));
+    System.out.println(board.piece_at(2,3));
   }
 }
