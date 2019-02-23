d3e954fb534c6176fe870e1549ee7ae1d61ab2d3
wur3 <wu.richard.98@gmail.com>
2019-02-23 16:01:24 -0500

    Bishop movements

--- Bishop.java
+++ Bishop.java
@@ -6,6 +6,19 @@ public class Bishop extends Piece{
 
   @Override
   public void move(int x, int y) {
+    if (!is_diagonal(x,y)) {
+      System.out.println("Bishops cannot move like that!");
+    }
+    else {
+      if (diagonal_path(x, y, true)) {
+        atomic_move(x, y);
+      }
+      //space is occupied by enemy
+      else if (diagonal_path(x, y, false) && b.piece_at(x,y).getColor() != color) {
+        b.clear(x, y);
+        atomic_move(x, y);
+      }
+    }
     return;
   }
 
