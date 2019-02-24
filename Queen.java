8715e801dd967df20ebcfdd0a52f435a59cf56cd
wur3 <wu.richard.98@gmail.com>
2019-02-23 19:03:47 -0500

    Queen movement

--- Queen.java
+++ Queen.java
@@ -6,6 +6,40 @@ public class Queen extends Piece{
 
   @Override
   public void move(int x, int y) {
+    if (is_diagonal(x,y)) {
+      if (diagonal_path(x, y, true)) {
+        atomic_move(x, y);
+      }
+      //space is occupied by enemy
+      else if (diagonal_path(x, y, false) && b.piece_at(x,y).getColor() != color) {
+        b.clear(x, y);
+        atomic_move(x, y);
+      }
+    }
+    //horizontal moving
+    else if ((this.x != x) && (this.y == y)) {
+      //targetted space is empty
+      if (horizontal_path(x, true)) {
+        atomic_move(x, y);
+      }
+      //space is occupied by enemy
+      else if (horizontal_path(x, false) && b.piece_at(x,y).getColor() != color) {
+        b.clear(x, y);
+        atomic_move(x, y);
+      }
+    }
+    //vertical moving
+    else if ((this.x == x) && (this.y != y)) {
+      //targetted space is empty
+      if (vertical_path(y, true)) {
+        atomic_move(x, y);
+      }
+      //space is occupied by enemy
+      else if (vertical_path(y, false) && b.piece_at(x,y).getColor() != color) {
+        b.clear(x, y);
+        atomic_move(x, y);
+      }
+    }
     return;
   }
 
